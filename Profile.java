a6bae46258d2ad667afa2ea5c4d9e7a6399aa46b
ONEcam <44405285+ONEcam@users.noreply.github.com>
2020-01-17 23:14:01 -0500

    Create Profile.java

---
+++ Profile.java
@@ -0,0 +1,17 @@
+
+public interface Profile {
+	
+	public String getFirstName();
+	
+	public String getLastname();
+	
+	public String getNacionality();
+	
+	public String getRacingClass();
+	
+	public String getRacingChassis();
+	
+	public String getRacingEngine();
+
+	
+}
